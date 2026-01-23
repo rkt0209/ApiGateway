b34f48d2b55147e6b466f4ed351744c967c5e0fe
Rohit Kumar <rohitdhankhar7347@gmail.com>
2026-01-23 19:57:51 +0530

    added middleware to authenticate user before accessign booking service

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const {createProxyMiddleware} = require('http-proxy-middleware')
 const  rateLimit = require('express-rate-limit');
-
+const axios = require('axios');
 const app = express();
 const PORT = 3005;
 const limiter = rateLimit({
@@ -12,6 +12,28 @@ const limiter = rateLimit({
 app.set('trust proxy', 1);
 app.use(morgan('combined'));
 app.use(limiter);
+
+// Middleware to authenticate the user before accessing bokking service
+app.use('/bookingservice',async(req,res,next)=>{
+   try {
+        const  response = await axios.get('http://localhost:7000/authservice/api/v1/isauthenticated',{
+            headers:{
+                "x-access-token":req.headers['x-access-token']
+            }
+        });
+        if(response.data.success){
+            next();
+        }else{
+            return res.status(401).json({
+                message:"Unauthorised user"
+            })
+        }
+   } catch (error) {
+     return res.status(401).json({
+        message:"Unauthorised user"
+     })
+   } 
+})
 
 app.use('/bookingservice',
   createProxyMiddleware({
