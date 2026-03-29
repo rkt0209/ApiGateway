40ed2cc5486b1374aabee7bec01b807036646fd0
Rohit Kumar <rohitdhankhar7347@gmail.com>
2026-03-29 12:39:25 +0530

    reset the rate limiter to 1000 request per min

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const axios = require('axios');
 const app = express();
 const PORT = 3005;
 const limiter = rateLimit({
-    windowMs:2 * 60 * 1000,
-    max: 5
+    windowMs: 1 * 60 * 1000,
+    max: 1000
 })
 app.set('trust proxy', 1);
 app.use(morgan('combined'));
